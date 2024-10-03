154ea4247f6b5053395b0525577f91d00da14832
JMTuggle <jmtuggle@uw.edu>
2024-10-03 10:33:29 -0700

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,49 +1,48 @@
 import numpy as np
-x = np.array([-1.6])
+A1 = [-1.6]
 for j in range(100):
-    f_xj = x[j] * np.sin(3 * x[j]) - np.exp(x[j])
-    f_der_xj = np.sin(3 * x[j]) + 3 * x[j] * np.cos(3 * x[j]) - np.exp(x[j])
-    x_new = x[j] - f_xj / f_der_xj
-    x = np.append(x, x_new)
-    fc=x[j+1]*np.sin(3*x[j+1])-np.exp(x[j+1])
+    f_xj = A1[j] * np.sin(3 * A1[j]) - np.exp(A1[j])
+    f_der_xj = np.sin(3 * A1[j]) + 3 * A1[j] * np.cos(3 * A1[j]) - np.exp(A1[j])
+    x_new = A1[j] - f_xj / f_der_xj
+    A1 = np.append(A1, x_new)
+    fc=A1[j]*np.sin(3*A1[j])-np.exp(A1[j])
     if abs(fc) < 1e-6:
-        display(j)
         break 
-x
+iter_1 = j
 
 xr = -0.4
 xl = -0.7
-mid = np.array([])
+xbi = []
 
 for j in range(100):
     xc = (xr + xl)/2
-    mid = np.append(mid, xc) 
+    xbi.append(xc) 
     fc = (xc) * np.sin(3*(xc)) - np.exp(xc)
     if ( fc > 0 ):
         xl = xc
     else:
         xr = xc
     if  abs(fc) < 1e-6:
-        display(j)
         break
-mid
+iter_2 = j
+A2 = xbi
 
-A3 = np.array([9, 16])
+A3 = [iter_1 + 1 , iter_2 + 1]
 
 A = np.array([[1, 2] , [-1, 1]])
 B = np.array([[2, 0] , [0, 2]])
 C = np.array([[2,0,-3],[0,0,-1]])
 D = np.array([[1,2],[2,3],[-1,0]])
-x = np.array([[1],[0]])
-y = np.array([[0],[1]])
-z = np.array([[1],[2],[-1]])
+x = np.array([1 , 0])
+y = np.array([0 , 1])
+z = np.array([1, 2, -1])
 
 A4 = A + B
 A5 = 3*x - 4*y
 A6 = A @ x
 A7 = B @(x-y)
 A8 = D@x
-A9 = D@x+z
+A9 = D@y+z
 A10 = A@B
 A11 = B @ C
 A12 = C @ D
